0c9dbd0a4b43b486830209e8fe5e93a128fca0f9
Nathanael Jones <nathanael.jones@gmail.com>
2016-06-13 14:15:42 -0600

    Missing js

---
+++ js/all-except-main-and-ie8.js
@@ -0,0 +1,10 @@
+---
+---
+
+{% include_relative jquery.min.js %}
+{% include_relative jquery.scrollex.min.js %}
+
+{% include_relative jquery.scrolly.min.js %}
+
+{% include_relative skel.min.js %}
+{% include_relative util.js %}
