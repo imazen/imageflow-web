1967b013bb4a168632eea6fd7f8e29014011dfe7
anshul <anshulgoel151999@gmail.com>
2021-01-30 03:53:47 +0530

    fix code samples

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,36 +19,44 @@ const samples = [
     --response operation_result.json
 `,
     language: "bash",
+    addtionalComponents: <></>,
   },
   {
     tab: "Querystring",
     code: `/demo_images/tulip-leaf.jpg?w=300&h=300&mode=max`,
     language: "bash",
+    addtionalComponents: <></>,
   },
   {
     tab: "Dotnet",
-    code: `using Imageflow.Fluent;
-
-    public async void TestGetImageInfo()
-    {
-        var imageBytes = Convert.FromBase64String(
-            "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEX/TQBcNTh/AAAAAXRSTlPM0jRW/QAAAApJREFUeJxjYgAAAAYAAzY3fKgAAAAASUVORK5CYII=");
-    
-        var info = await ImageJob.GetImageInfo(new BytesSource(imageBytes));
-        
-        Assert.Equal(info.ImageWidth, 1);
-        Assert.Equal(info.ImageHeight, 1);
-        Assert.Equal(info.PreferredExtension, "png");
-        Assert.Equal(info.PreferredMimeType, "image/png");
-    }`,
+    code: `using (var b = new ImageJob()) {
+      await b.Decode(inputStream, true)
+              .ConstrainWithin(800, 800)
+              .Encode(new StreamDestination(outputStream, false), new WebPLossyEncoder(80))
+              .Finish()
+              .InProcessAsync();
+   }`,
     language: "java",
+    addtionalComponents: (
+      <>
+        <a href="https://github.com/imazen/imageflow-dotnet">
+          Install the Imageflow.Net and Imageflow.NativeRuntime.* packages
+        </a>
+      </>
+    ),
   },
   {
     tab: "Dotnet Server",
     code: `app.UseImageflow(new ImageflowMiddlewareOptions()
     .SetMapWebRoot(true)
-    .SetMyOpenSourceProjectUrl("https://github.com/my/project"));`,
+    .SetMyOpenSourceProjectUrl("https://github.com/my/project"))`,
     language: "java",
+    addtionalComponents: (
+      <>
+        Don't forget to install Imageflow.Server
+        <code> donet add package Imageflow.Server </code>
+      </>
+    ),
   },
   {
     tab: "Nodejs",
@@ -56,35 +64,11 @@ const samples = [
       MozJPEG,
       Steps,
       FromURL,
-      FromFile,
-      FromStream,
       FromBuffer,
   } = require('@imazen/imageflow')
   const fs = require('fs')
   
   let step = new Steps(new FromURL('https://jpeg.org/images/jpeg2000-home.jpg'))
-      .constrainWithin(500, 500)
-      .branch((step) =>
-          step
-              .constrainWithin(400, 400)
-              .branch((step) =>
-                  step
-                      .constrainWithin(200, 200)
-                      .rotate90()
-                      .colorFilterGrayscaleFlat()
-                      .encode(new FromFile('./branch_2.jpg'), new MozJPEG(80))
-              )
-              .copyRectangle(
-                  (canvas) =>
-                      canvas.decode(
-                          new FromStream(fs.createReadStream('./test.jpg'))
-                      ),
-                  { x: 0, y: 0, w: 100, h: 100 },
-                  10,
-                  10
-              )
-              .encode(new FromFile('./branch.jpg'), new MozJPEG(80))
-      )
       .constrainWithin(100, 100)
       .rotate180()
   step.encode(new FromBuffer(null, 'key'), new MozJPEG(80))
@@ -92,6 +76,7 @@ const samples = [
       .then(console.log)
       .catch(console.log)`,
     language: "js",
+    addtionalComponents: <></>,
   },
   {
     tab: "Golang",
@@ -107,61 +92,15 @@ const samples = [
       step:=imageflow.NewStep()
       data,_:=step
       .Decode(imageflow.NewURL("https://jpeg.org/images/jpeg2000-home.jpg"))
-      .Branch(func(step *imageflow.Steps){
-        step
-        .ConstrainWithin(200,200)
-        .Encode(imageflow.NewFile("test_1.jpg"),imageflow.MozJPEG{})
-      }).ConstrainWithin(400,400)
+      .ConstrainWithin(400,400)
       .Encode(imageflow.GetBuffer("test"),imageflow.MozJPEG{})
       .Execute()
       ioutil.WriteFile("test_2.jpeg",data["test"],0644)
     }`,
     language: "go",
+    addtionalComponents: <></>,
   },
 ];
-
-const features = [
-  {
-    title: "Process multiple image",
-    imageUrl: "img/multiple.svg",
-    description: (
-      <>
-        Imageflow works on a directed graph for processing multiple images in
-        same operations. It allow the use of work done of other images and
-        create a branch from there.
-      </>
-    ),
-  },
-  {
-    title: "Perform complex operations",
-    imageUrl: "img/crop.svg",
-    description: (
-      <>
-        Imageflow supports multiple operations like crop, padding and many more.
-      </>
-    ),
-  },
-  {
-    title: "Resize on fly",
-    imageUrl: "img/resize.svg",
-    description: <>Resize image on fly using fast query based API.</>,
-  },
-];
-
-function Feature({ imageUrl, title, description }) {
-  const imgUrl = useBaseUrl(imageUrl);
-  return (
-    <div className={clsx("col col--4", styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
-        </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
-  );
-}
 
 function Home() {
   const context = useDocusaurusContext();
@@ -201,6 +140,7 @@ function Home() {
         <CodeBlock className={samples[currentTab].language}>
           {samples[currentTab].code}
         </CodeBlock>
+        {samples[currentTab].addtionalComponents}
       </div>
       <div className="container" style={{ marginTop: "40px" }}>
         <div class="row">
